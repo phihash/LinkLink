444062163844440f3e0a1c7a4101501f17569b42
phihash <insharp0220@gmail.com>
2024-03-25 03:19:01 +0900

    ログアウト/ログインできりかえ

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,29 @@
 "use client";
-import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../lib/firebase";
 import Header from "@/components/Header";
+import { noto_sans_jp } from "@/app/fonts";
+
 
 const Home = () => {
   const [user, loading] = useAuthState(auth);
 
-  const handleLogout = async () => {
-    await auth.signOut();
-  };
-
   if (loading) {
     return <div>ローディング中</div>;
   }
-
   return (
     <div>
       <Header></Header>
       {user ? (
         <>
-          <h1>Welcome, {user.displayName}!</h1>
-          <button onClick={handleLogout}>Logout</button>
+        <div className="w-11/12 pt-10 text-slate-700 mx-auto">
+          <h1 className={`${noto_sans_jp.className} text-2xl font-semibold`}>用意された質問</h1>
+        <h2>Welcome, {user.displayName}!</h2>
+
+        </div>
         </>
       ) : (
-        <>
-        </>
+        <></>
       )}
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,43 @@
 import React from "react";
 import Link from "next/link";
 import { noto_sans_jp } from "@/app/fonts";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebase";
 
 const Header = () => {
+  const [user, loading] = useAuthState(auth);
+  const handleLogout = async () => {
+    await auth.signOut();
+  };
+
   return (
     <div className="bg-slate-100 py-4 md:py-6 flex items-center justify-between">
-      <h1 className="text-2xl md:text-3xl font-bold pl-8">
-      <Link href="/">App</Link>
-        </h1>
-        <Link href="/login" rel="noopener noreferrer"
-                  target="_blank">
-        <button className={`${noto_sans_jp.className} font-semibold mr-6 bg-sky-400 md:py-3 md:px-8 px-6 py-3 md:text-base text-sm  rounded-xl text-white `}>
-         ログイン
-        </button>
-        </Link>
+      {user ? (
+        <>
+          <h1 className="text-2xl md:text-3xl font-bold pl-8">
+            <Link href="/">App</Link>
+          </h1>
+          <button
+            onClick={handleLogout}
+            className={`${noto_sans_jp.className} font-semibold mr-6 bg-sky-400 md:py-3 md:px-8 px-6 py-3 md:text-base text-sm  rounded-xl text-white `}
+          >
+            ログアウト
+          </button>
+        </>
+      ) : (
+        <>
+          <h1 className="text-2xl md:text-3xl font-bold pl-8">
+            <Link href="/">App</Link>
+          </h1>
+          <Link href="/login" rel="noopener noreferrer" target="_blank">
+            <button
+              className={`${noto_sans_jp.className} font-semibold mr-6 bg-sky-400 md:py-3 md:px-8 px-6 py-3 md:text-base text-sm  rounded-xl text-white `}
+            >
+              ログイン
+            </button>
+          </Link>
+        </>
+      )}
     </div>
   );
 };
